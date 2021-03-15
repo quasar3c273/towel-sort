6802afa8598331e739b9e60dfdc9555f92c3267f
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-15 13:07:22 +0300

    init: if !maxtrix
    init: if !maxtrix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 module.exports = function towelSort (matrix) {
+    if ( !matrix ) {
+        return []
+    }
     if ( matrix ) {
         let newArr = [];
-        for(let i = 0; i < matrix.length; i++) {
+        for(let i=0; i < matrix.length; i++) {
             if (i % 2 === 0) {
                 newArr=newArr.concat(matrix[i])
             }
